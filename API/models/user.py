7bf39f59992f7a760c6be5e6377a43816232556d
Victor <hellovictorlee@gmail.com>
2019-03-03 00:01:10 -0800

    add: user database model

---
+++ API/models/user.py
@@ -0,0 +1,37 @@
+from models.database import db
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+    def json(self):
+        return {'id': self.id,
+                'name': self.name}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_id(cls, user_id):
+        return cls.query.filter_by(id=user_id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def add_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
