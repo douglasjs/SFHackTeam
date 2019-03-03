084d4038e9f302d7df46a993e48affafd8832393
Victor <hellovictorlee@gmail.com>
2019-03-03 00:00:27 -0800

    modify: database implementation

--- API/resources/users.py
+++ API/resources/users.py
@@ -1,40 +1,33 @@
-from flask_restful import reqparse, abort, Resource
-from db import USERS
-
-
-def abort_if_user_doesnt_exist(user_id):
-    if user_id not in USERS:
-        abort(404, message="User {} doesn't exist".format(user_id))
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
+from flask_restful import reqparse, Resource
+from models.user import UserModel
 
 
 class User(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+                        type=str,
+                        required=True,
+                        help='This field can\'t be blank')
+
     def get(self, user_id):
-        abort_if_user_doesnt_exist(user_id)
-        return USERS[user_id]
-
-    def delete(self, user_id):
-        abort_if_user_doesnt_exist(user_id)
-        del USERS[user_id]
-        return '', 204
-
-    def put(self, user_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        USERS[user_id] = task
-        return task, 201
+        user = UserModel.find_by_id(user_id)
+        if user:
+            return user.json()
+        return {'msg': 'User not found'}, 404
 
 
 class UserList(Resource):
     def get(self):
-        return USERS
+        users = list(map(lambda x: x.json(), UserModel.find_all()))
+        return {'users': [user for user in users]}, 200
 
     def post(self):
-        args = parser.parse_args()
-        user_id = int(max(USERS.keys()).lstrip('user')) + 1
-        user_id = 'user%i' % user_id
-        USERS[user_id] = {'task': args['task']}
-        return USERS[user_id], 201
+        # parse client input data
+        data = User.parser.parse_args()
+        name = data['name']
+        if UserModel.find_by_name(name):
+            return {'msg': '{} already exist'.format(name)}, 400
+        # create user info into database
+        user = UserModel(**data)
+        user.add_to_db()
+        return {'msg': 'success'}, 201
