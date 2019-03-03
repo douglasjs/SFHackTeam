9852f45401a9755701474784463ed4f44042884b
Victor <hellovictorlee@gmail.com>
2019-03-02 23:57:46 -0800

    add: database implementation

--- API/app.py
+++ API/app.py
@@ -5,10 +5,23 @@ from resources.users import UserList, User
 app = Flask(__name__)
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sfhacks.db'
+
+
 # Actually setup the Api resource routing here
 api.add_resource(UserList, '/users')
 api.add_resource(User, '/users/<user_id>')
 
+# api.add_resource(UserList, '/stores')
+# api.add_resource(User, '/stores/<store_id>')
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from models.database import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
